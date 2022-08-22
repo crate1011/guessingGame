961d3704c869a868252a67f23b69a50b0df2f615
Christian Suriano <cratea113@gmail.com>
2022-08-22 08:26:20 -0700

    phase 2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 
 Console.WriteLine("HI! Guess the secret number!");
-Console.Write("What's your guess?");
+Console.Write("What's your guess? ");
 
 string input = Console.ReadLine();
 
 int numberGuess = int.Parse(input);
 
-Console.WriteLine(numberGuess);
+int secretNumber = 42;
+
+if (numberGuess == secretNumber)
+{
+    Console.WriteLine("You Guessed It!!!");
+}
+else
+{
+    Console.WriteLine("You Shall Not Pass!!!");
+}
