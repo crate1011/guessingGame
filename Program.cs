670f9dd0a8104a6bf4b092862cf4711eee8f701b
Christian Suriano <cratea113@gmail.com>
2022-08-22 09:18:04 -0700

    phase 3

--- Program.cs
+++ Program.cs
@@ -9,11 +9,40 @@ int numberGuess = int.Parse(input);
 
 int secretNumber = 42;
 
+void tryAgain()
+{
+    Console.WriteLine("You Shall Not Pass!!!");
+    Console.Write("What's your guess? ");
+    input = Console.ReadLine();
+    numberGuess = int.Parse(input);
+}
+
 if (numberGuess == secretNumber)
 {
     Console.WriteLine("You Guessed It!!!");
 }
 else
 {
-    Console.WriteLine("You Shall Not Pass!!!");
+    tryAgain();
+    if (numberGuess == secretNumber)
+    {
+        Console.WriteLine("You Guessed It!!!");
+    }
+    else
+    {
+        tryAgain();
+        if (numberGuess == secretNumber)
+        {
+            Console.WriteLine("You Guessed It!!!");
+        }
+        else
+        {
+            tryAgain();
+            if (numberGuess == secretNumber)
+            {
+                Console.WriteLine("You Guessed It!!!");
+            }
+        }
+    }
+
 }
