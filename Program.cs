a775d7b3e99031df77ac958fa863ebde217437b5
Christian Suriano <cratea113@gmail.com>
2022-08-22 08:13:31 -0700

    first phase

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+Console.WriteLine("HI! Guess the secret number!");
+Console.Write("What's your guess?");
+
+string input = Console.ReadLine();
+
+int numberGuess = int.Parse(input);
+
+Console.WriteLine(numberGuess);
